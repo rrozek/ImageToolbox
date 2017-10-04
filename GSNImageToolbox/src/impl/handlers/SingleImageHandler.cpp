2f1b8eecd79b362e6cf0f5a5e309da7ed17612f4
Jakub Rojek <jakub.rojek.ext@nokia.com>
2017-10-04 11:41:02 +0200

    Working clip path for TIFF and EPS

--- GSNImageToolbox/src/impl/handlers/SingleImageHandler.cpp
+++ GSNImageToolbox/src/impl/handlers/SingleImageHandler.cpp
@@ -37,36 +37,13 @@ char *SingleImageHandler::getImage(quint8 imageNumber, common::EImageFormat form
     try
     {
         Magick::Blob outputBlob;
-//        m_sourceImage->verbose(true);
-//        Magick::Blob imageProfiles = m_sourceImage->profile("ICM");
-//        qDebug() << QByteArray::fromBase64(QByteArray::fromStdString(imageProfiles.base64()));
-
-//        GetPPException;
-
-//        GetImageProperty(m_sourceImage->constImage(),"exif:*",exceptionInfo);
-//        GetImageProperty(m_sourceImage->constImage(),"icc:*",exceptionInfo);
-//        GetImageProperty(m_sourceImage->constImage(),"iptc:*",exceptionInfo);
-//        GetImageProperty(m_sourceImage->constImage(),"xmp:*",exceptionInfo);
-//        ResetImagePropertyIterator(m_sourceImage->constImage());
-//        const char* property = GetNextImageProperty(m_sourceImage->constImage());
-//        while ( property != static_cast<const char*>(NULL))
-//        {
-//            const char* value = GetImageProperty(m_sourceImage->constImage(),property,exceptionInfo);
-//            qDebug() << property << value;
-//            property = GetNextImageProperty(m_sourceImage->constImage());
-//        }
-        m_sourceImage->fileName("json:");
+        applyMaskFromClippingPath(*m_sourceImage,format);
         m_sourceImage->write(&outputBlob);
 
         dataSize = outputBlob.length();
 
         char* returnArray = new char[dataSize];
         memcpy(returnArray, static_cast<const char*>(outputBlob.data()), outputBlob.length());
-        QFile file("identify.txt");
-        file.open(QIODevice::WriteOnly | QIODevice::Truncate);
-        file.write(returnArray);
-        file.close();
-//        qDebug() << returnArray;
         return returnArray;
     }
     catch( Magick::Exception &error)
@@ -83,21 +60,18 @@ void SingleImageHandler::handleSource()
     try
     {
         m_sourceImage = std::make_unique<Magick::Image>();
-        qDebug() << m_sourceImage->density().x() << m_sourceImage->density().y();
-        qDebug() << m_imageInfo->getValue("root[0].image.resolution.x").toInt();
-        qDebug() << m_imageInfo->getValue("root[0].image.resolution.y").toInt();
         m_sourceImage->density(Magick::Point(m_imageInfo->getValue("root[0].image.resolution.x").toInt(),m_imageInfo->getValue("root[0].image.resolution.y").toInt()));
-        qDebug() << m_sourceImage->density().x() << m_sourceImage->density().y();
-        qDebug() << m_sourceImage->geometry().width() << m_sourceImage->geometry().height();
-        m_sourceImage->read(*m_sourceBlob);
-        qDebug() << "abc";
-        m_sourceImage->write("dupa.png");
-        qDebug() << "def";
+        m_sourceImage->read(*m_sourceBlob, Magick::Geometry(m_imageInfo->getValue("root[0].image.geometry.width").toInt(),
+                                                            m_imageInfo->getValue("root[0].image.geometry.height").toInt(),
+                                                            m_imageInfo->getValue("root[0].image.geometry.x").toInt(),
+                                                            m_imageInfo->getValue("root[0].image.geometry.y").toInt()
+                                                            ), m_imageInfo->getValue("root[0].image.deptrh").toInt()
+                            );
     }
-    catch( ... )
+    catch( Magick::Exception &error)
     {
         qWarning() << Q_FUNC_INFO << "caught ImageMagick exception";
-//        qWarning() << error.what();
+        qWarning() << error.what();
     }
 }
 
@@ -105,9 +79,15 @@ void SingleImageHandler::applyMaskFromClippingPath(Magick::Image &image, common:
 {
     try
     {
+        QVariant clipPath = m_imageInfo->getValue("root[0].image.clipping path");
+        if (clipPath.isValid())
+        {
         image.alphaChannel(MagickCore::TransparentAlphaChannel);
         image.clip();
         image.alphaChannel(MagickCore::OpaqueAlphaChannel);
+        }
+        else
+            qDebug() << "no clip path present.";
 
         image.magick(common::EImageFormatString[format]);
     }
